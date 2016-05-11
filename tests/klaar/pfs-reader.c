0df6bef36bf47230fd0fcc39151240c402a831fa
oscis711 <oscis711@student.liu.se>
2016-05-11 15:29:09 +0200

    Får inte pintos uppdateringen att fungera

---
+++ tests/klaar/pfs-reader.c
@@ -0,0 +1,45 @@
+/* Part of pfs.c suite.
+   
+   Reads from the file and checks consistency.
+   The buffer should all contain the same character!!
+ */
+
+#include <syscall.h>
+#include <stdio.h>
+
+#include "../lib.h"
+#include "pfs.h"
+
+static bool check_consistency(char* buf, int size)
+{
+  int i = 1;
+  for (i; i < size; ++i)
+  {
+    if (buf[0] != buf[i])
+    {
+      /* Ooops, inconsistency */
+      return false;
+    }
+  }
+  return true;
+}
+
+char buffer[BIG];
+
+int main (int argc UNUSED, char *argv[]) 
+{
+  test_name = argv[0];
+  quiet = true;
+  int i = 0;
+  for (i; i < TIMES; ++i)
+  {
+    int id = open ("file.1");
+    CHECK ( id > 1, "open \"file.1\"");    
+    int bytes = read(id, buffer, BIG);
+    CHECK ( bytes == BIG, "read \"file.1\"");    
+    close(id);
+
+    CHECK ( check_consistency(buffer, BIG), "inconsistency");
+  }
+  return 0;
+}
